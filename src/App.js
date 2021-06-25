1ee8a53167c9799c9ef1654810d5e8f0b17ca3a4
Khushi Saxena <khushisaxena123123@gmai;.com>
2021-06-25 18:37:38 +0530

    Add Assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import GlobalStyle from "./components/globalStyles";
+import Hero from "./components/Hero";
+import { SliderData } from "./data/SliderData";
+import Dropdown from "./components/Dropdown";
 
 function App() {
+  //   const [isOpen, setUsOpen] = useState(false);
+
+  //   const toggle = () => {
+  //     setIsOpen(!isOpen);
+  //   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar />
+      {/* <Dropdown isOpen={isOpen} toggle={toggle} /> */}
+      <Hero slides={SliderData} />
+    </>
   );
 }
 
---
+++ src/data/SliderData.js
@@ -0,0 +1,38 @@
+import ImageOne from "../images/Houseone.jpg";
+import ImageTwo from "../images/Housetwo.jpg";
+import ImageThree from "../images/Housethree.jpg";
+import ImageFour from "../images/Housefour.jpg";
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in California",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "Luxury Villa in Newyork",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
